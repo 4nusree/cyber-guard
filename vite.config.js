d5fa9d6d4b58b3a781775a559fbc1596f91bce0e
Anusree S Nair <sreeanu0605@gmail.com>
2026-02-24 16:05:02 +0530

    added code base

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  root: "client",
+  server: {
+    allowedHosts: true,
+    host: "0.0.0.0",
+    port: 5000,
+    hmr: {
+      clientPort: 443,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client/src"),
+    },
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+});
